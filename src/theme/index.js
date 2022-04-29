51b7bd5b4bb070570aa4c0534bcb75bdef882234
zhaozhiyuan@hcfysz.com <zhaozhiyuan@hcfysz.com>
2022-04-29 16:32:15 +0800

    11

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,6 +1,6 @@
 // Ant Design 主题配置  修改需重启
 module.exports = {
-    'primary-color': '#ffbac7',  // 使用动态换肤插件就不要在这里使用颜色
+    "primary-color": "#ffbac7", // 使用动态换肤插件就不要在这里使用颜色
     // // 'border-radius-base':'4px'
     // "font-family": "PingFangSC-Regular !important",
     // "input-height-base": "28px",
